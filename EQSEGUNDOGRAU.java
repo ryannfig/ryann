c91a24b2f8dcabde97b33f4a031481c6df99c6e2
ryannfig <113732663+ryannfig@users.noreply.github.com>
2023-08-23 19:03:11 -0300

    Update EQSEGUNDOGRAU.java

--- EQSEGUNDOGRAU.java
+++ EQSEGUNDOGRAU.java
@@ -1,23 +1,28 @@
-package eqseggrau;
+package delta;
 import javax.swing.JOptionPane;
-public class EQSEGGRAU {
 
+public class Delta {
+
+  
     public static void main(String[] args) {
-        int A, B, C, EQ;
-        double raiz;
-        String sA, sB, sC;
-        
-        sA = JOptionPane.showInputDialog(null, "Escreva o primeiro numero: ");
-        sB = JOptionPane.showInputDialog(null, "Escreva o segundo numero: ");
-        sC = JOptionPane.showInputDialog(null, "Escreva o terceiro numero: ");
-        
-        A = Integer.parseInt(sA);
-        B = Integer.parseInt(sB);
-        C = Integer.parseInt(sC);
-         
-        EQ = (B*B)-4*(A)*(C);
-        raiz = Math.sqrt(EQ);
-        JOptionPane.showMessageDialog(null, "O resultado da equação é: " +raiz);
+      double A,B,C,X1,X2,delta,raiz;
+      String sA, sB, sC;
+      
+      sA = JOptionPane.showInputDialog(null, "Digite o valor de A: ");
+      sB = JOptionPane.showInputDialog(null, "Digite o valor de B ");
+      sC = JOptionPane.showInputDialog(null, "Digite o valor de C: ");
+      
+      A = Integer.parseInt(sA);
+      B = Integer.parseInt(sB);
+      C = Integer.parseInt(sC);
+      delta = (B*B)-4*A*C;
+      raiz = Math.sqrt(delta);
+      
+      X1 = (-B)+raiz/2*A;
+      X2 = (-B)-raiz/2*A;
+      
+      JOptionPane.showMessageDialog(null," O resultado de x1 é: " +X1+ " O resultado de x2 é: " +X2);
     }
     
 }
+
