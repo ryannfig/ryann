eb750e1c3a1c9e498ec40a9fb5bc73b512372f1f
ryannfig <113732663+ryannfig@users.noreply.github.com>
2023-08-24 09:17:23 -0300

    Create Bomdia.java

---
+++ Bomdia.java
@@ -0,0 +1,16 @@
+package bomdia;
+import javax.swing.JOptionPane;
+public class Bomdia {
+
+    public static void main(String[] args) {
+       double hora = 7;
+      //String shora;
+       
+      //shora = JOptionPane.showInputDialog(null, "Digite a hora");
+       
+       //hora = Double.parseDouble(shora);
+       
+       JOptionPane.showMessageDialog(null, "Bom dia, são " +hora+ " horas ");
+    }
+    
+}
